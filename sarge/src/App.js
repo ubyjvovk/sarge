1f1635820641cde832cc52a386c34d47fe7cd720
dmitryyy <darveter9@gmail.com>
2023-02-14 17:54:22 -0500

    kittentab

--- sarge/src/App.js
+++ sarge/src/App.js
@@ -2,17 +2,8 @@ import React, { useState, useEffect, useCallback } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Tabs, Tab } from 'react-bootstrap';
 
-// const voices = [];
-// const populateVoices = () => {
-//   voices.length = 0;
-//   console.log("cleaned voices:", voices);
-//   for(const voice of window.speechSynthesis.getVoices()) voices.push(voice);
-//   console.log("loaded voices:", voices);
-// };
-
-
 const App = () => {
-  
+
   const [isSwitchOn, setIsSwitchOn] = useState(false);
   const [interval, setNotifInterval] = useState(
     parseInt(localStorage.getItem('interval')) || 5
@@ -29,14 +20,11 @@ const App = () => {
   console.log('NEW RENDER with voices:', voices);
 
   if (voices.length === 0 && window.speechSynthesis.onvoiceschanged !== undefined) {
-    console.log("setting timeout");
-    setTimeout(()=> {
+    console.log("will wait for voices to load");
+    setTimeout(() => {
       setVoices(window.speechSynthesis.getVoices());
     }, 25);
   }
- 
-  // getVoices();  // Firefox needs an attempt to get voices before populating them
-  // window.speechSynthesis.onvoiceschanged = getVoices;
 
   useEffect(() => {
     localStorage.setItem('interval', interval);
@@ -44,27 +32,13 @@ const App = () => {
     localStorage.setItem('message', message);
   }, [interval, selectedVoice, message]);
 
-  // const shout3 = useCallback(() => {
-  //     const utterance = new SpeechSynthesisUtterance(message);
-  //     utterance.voice = voices.find(voice => voice.name === selectedVoice);
-  //     console.log(voices, selectedVoice);
-  //     console.log("uuuu", utterance);
-  //     window.speechSynthesis.speak(utterance);
-  //   }, [selectedVoice, message, voices]);
-
   const shout = useCallback((e) => {
-      if (e) e.preventDefault();
-      const utterance = new SpeechSynthesisUtterance(message);
-      utterance.voice = voices.find(voice => voice.name === selectedVoice);
-      // console.log(voices, selectedVoice);
-      // console.log("uuuu", utterance);
-      console.log("Shout:", message);
-      window.speechSynthesis.speak(utterance);
+    if (e) e.preventDefault();
+    const utterance = new SpeechSynthesisUtterance(message);
+    utterance.voice = voices.find(voice => voice.name === selectedVoice);
+    console.log("Shout:", message);
+    window.speechSynthesis.speak(utterance);
   }, [voices, selectedVoice, message]);
-  // window.speechSynthesis.getVoices(); //because Firefox
-  // setTimeout(()=>{setVoices(window.speechSynthesis.getVoices())}, 10);
-
-    // 
 
 
   useEffect(() => {
@@ -76,7 +50,7 @@ const App = () => {
       intervalId = setTimeout(() => {
         shout();
         setYellCount(yellCount + 1); //also rearms the notification, as this useEffect depends on yellCount
-      }, randomInterval*1000);  // because milliseconds
+      }, randomInterval * 1000);  // because milliseconds
     } else {
       clearInterval(intervalId);
     }
@@ -85,7 +59,7 @@ const App = () => {
     };
   }, [isSwitchOn, interval, yellCount, message, selectedVoice, voices, shout]); // no need to depend on message and voice, shout deps on them
 
-  
+
   const handleSwitchChange = () => {
     setIsSwitchOn(!isSwitchOn);
   };
@@ -102,15 +76,18 @@ const App = () => {
     setMessage(event.target.value);
   };
   return (
-    <div className="container mt-5">
+    <div className="container-sm mt-5">
       <Tabs defaultActiveKey="main" id="uncontrolled-tab-example">
+
+
+
         <Tab eventKey="main" title="Drill Sergeant">
           <h2 className="text-center mt-5">Drill Sergeant</h2>
           <p className="mt-3">
-          This is a programmable notification timer that reminds you to stay focused and on track while working. 
+            This is a programmable notification timer that reminds you to stay focused and on track while working.
           </p>
           <p className="mt-3">
-          Sarge will yell at you periodically (at random, around the interval you set), and you will obey.
+            Sarge will yell at you periodically (at random, around the interval you set), and you will obey.
           </p>
           <p>
             {isSwitchOn ? `The heat is on! Will yell every ${interval} minutes on average, ${yellCount} yells so far` : "Sarge is asleep"}
@@ -124,58 +101,100 @@ const App = () => {
             </button>
           </div>
         </Tab>
+
+        <Tab eventKey="hero" title='hero'>
+          <div class="container my-5">
+            <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+              <div class="col-lg-5 p-3 p-lg-5 pt-lg-3">
+                <h1 class="display-4 fw-bold lh-1 mb-5">Your Personal Drill Sergeant</h1>
+                <p class='lead'> Sarge will keep you fit and productive</p>
+                <p class='lead'>
+                  He will check on you periodically (at random, around the interval you set), and if you're not working, you must pay the price.
+                </p>
+                
+                <p class='lead'>
+                In other words, this is a programmable notification timer that reminds you to stay focused and on track.
+                </p>
+
+
+                <p class='lead mb-5 fw'>
+                  Sarge is asleep.
+                </p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+                  
+                  
+                  <button type="button" 
+                      className={`btn btn-lg px-4 me-md-2 fw-bold btn-${isSwitchOn ? "danger" : "success"} btn-lg`}
+                      onClick={handleSwitchChange}
+                  >
+                    {isSwitchOn ? "Turn Off" : "Turn On"}
+                  </button>
+                  
+                  
+                  <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                </div>
+              </div>
+              <div class="col-lg-5 offset-lg-2 p-0 overflow-hidden shadow-lg">
+                <img class="rounded-lg-3" src="/sarge/sarge2.png" alt="" width='512' />
+              </div>
+            </div>
+          </div>
+        </Tab>
+
+
+
         <Tab eventKey="settings" title="Settings">
           <form className='form-inline'>
 
-          <h2 className="text-center mt-5">Settings</h2>
+            <h2 className="text-center mt-5">Settings</h2>
 
-          <div className="form-group mt-5">
-            <label htmlFor="intervalRange">Interval (minutes): </label>
-            <input
-              type="range"
-              className="form-control-range"
-              id="intervalRange"
-              min={1}
-              max={300}
-              value={interval}
-              onChange={handleIntervalChange}
-            />
-            {interval}
-          </div>
+            <div className="form-group mt-5">
+              <label htmlFor="intervalRange">Interval (minutes): </label>
+              <input
+                type="range"
+                className="form-control-range"
+                id="intervalRange"
+                min={1}
+                max={300}
+                value={interval}
+                onChange={handleIntervalChange}
+              />
+              {interval}
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="voiceSelect">Voice:</label>
-            <select
-              className="form-control"
-              id="voiceSelect"
-              value={selectedVoice}
-              onChange={handleVoiceChange}
-            >
-              {voices.map(voice => (
-                <option key={voice.name} value={voice.name}>
-                  {voice.name} ({voice.lang})
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="messageInput">Message:</label>
-            <input
-              type="text"
-              className="form-control"
-              id="messageInput"
-              value={message}
-              onChange={handleMessageChange}
-            />
-          </div>
-          <div className="form-group">
-            <button formAction=''
-              className='btn btn-primary'
-              onClick={shout}
-            >
-              Try it
-            </button>
-          </div>
+            <div className="form-group">
+              <label htmlFor="voiceSelect">Voice:</label>
+              <select
+                className="form-control"
+                id="voiceSelect"
+                value={selectedVoice}
+                onChange={handleVoiceChange}
+              >
+                {voices.map(voice => (
+                  <option key={voice.name} value={voice.name}>
+                    {voice.name} ({voice.lang})
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="messageInput">Message:</label>
+              <input
+                type="text"
+                className="form-control"
+                id="messageInput"
+                value={message}
+                onChange={handleMessageChange}
+              />
+            </div>
+            <div className="form-group">
+              <button formAction=''
+                className='btn btn-primary'
+                onClick={shout}
+              >
+                Try it
+              </button>
+            </div>
           </form>
 
         </Tab>
